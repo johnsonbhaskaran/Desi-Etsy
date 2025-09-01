3e0e5702211abd72aca35e0fb81662209e33b1e4
Johnson Bhaskaran <johnsonb@live.in>
2025-09-01 16:48:58 +0530

    Aritisan Routes defined separately

--- backend/src/controllers/artisanRouteController.js
+++ backend/src/controllers/artisanRouteController.js
@@ -1,16 +1,15 @@
 import asyncHandler from "express-async-handler";
 
-const buyerLogin = asyncHandler(async (req, res) => {
-  res.json({ message: "buyer Login" });
-});
-const buyerRegister = asyncHandler(async (req, res) => {
-  res.json({ message: "buyer Register" });
-});
 const artisanLogin = asyncHandler(async (req, res) => {
   res.json({ message: "Artisan Login" });
 });
+
 const artisanRegister = asyncHandler(async (req, res) => {
   res.json({ message: "Artisan Register" });
 });
 
-export { buyerLogin, buyerRegister, artisanLogin, artisanRegister };
+const artisanCurrent = asyncHandler(async (req, res) => {
+  res.json({ message: "Artisan Current" });
+});
+
+export { artisanCurrent, artisanLogin, artisanRegister };
