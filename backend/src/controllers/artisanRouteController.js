5fb877c57483d63c679b96a5079ef67723e1e83d
Johnson Bhaskaran <johnsonb@live.in>
2025-09-01 17:17:29 +0530

    Proper comments added for buyer and artisan controllers

--- backend/src/controllers/artisanRouteController.js
+++ backend/src/controllers/artisanRouteController.js
@@ -1,13 +1,22 @@
 import asyncHandler from "express-async-handler";
 
+//*@desc Login as an Artisan
+//@route POST /api/auth/artisan/login
+//@access public
 const artisanLogin = asyncHandler(async (req, res) => {
   res.json({ message: "Artisan Login" });
 });
 
+//*@desc Register as an Artisan
+//@route POST /api/auth/artisan/register
+//@access public
 const artisanRegister = asyncHandler(async (req, res) => {
   res.json({ message: "Artisan Register" });
 });
 
+//*@desc Fetch current Artisan
+//@route GET /api/auth/artisan/current
+//@access public
 const artisanCurrent = asyncHandler(async (req, res) => {
   res.json({ message: "Artisan Current" });
 });
--- backend/src/controllers/buyerRouteController.js
+++ backend/src/controllers/buyerRouteController.js
@@ -1,11 +1,22 @@
 import asyncHandler from "express-async-handler";
 
+//*@desc Login as Buyer
+//@route POST /api/auth/buyer/login
+//@access public
 const buyerLogin = asyncHandler(async (req, res) => {
   res.json({ message: "buyer Login" });
 });
+
+//*@desc Register as Buyer
+//@route POST /api/auth/buyer/register
+//@access public
 const buyerRegister = asyncHandler(async (req, res) => {
   res.json({ message: "buyer Register" });
 });
+
+//*@desc Current Buyer
+//@route GET /api/auth/buyer/current
+//@access public
 const buyerCurrent = asyncHandler(async (req, res) => {
   res.json({ message: "buyer Current" });
 });
