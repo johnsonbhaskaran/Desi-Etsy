5e5a343226e1f07ae3798681bf6aaa236ec9e5c4
Johnson Bhaskaran <johnsonb@live.in>
2025-09-10 20:22:54 +0530

    Done: Categories component completed

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,8 +1,20 @@
 import styled from "styled-components";
+import { categories } from "../data";
+import CategoryItem from "./CategoryItem";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+`;
 
 const Categories = () => {
-  return <Container>Categories</Container>;
+  return (
+    <Container>
+      {categories.map((category, id) => (
+        <CategoryItem key={id} category={category} />
+      ))}
+    </Container>
+  );
 };
 export default Categories;
--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,8 +1,49 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const Info = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  border: none;
+  background-color: #fff;
+  color: gray;
+  font-weight: 600;
+  cursor: pointer;
+`;
 
-const CategoryItem = () => {
-  return <Container>CategoryItem</Container>;
+const CategoryItem = ({ category }) => {
+  return (
+    <Container>
+      <Image src={category.img} />
+      <Info>
+        <Title>{category.title}</Title>
+        <Button>SHOP NOW</Button>
+      </Info>
+    </Container>
+  );
 };
 export default CategoryItem;
