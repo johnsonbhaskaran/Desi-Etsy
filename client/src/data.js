63dad2f0156fb0da76de40cf500481098d5a9104
Johnson Bhaskaran <johnsonb@live.in>
2025-09-10 19:44:15 +0530

    Done: Slider component

---
+++ client/src/data.js
@@ -0,0 +1,22 @@
+import shoppingGirl from "../public/assets/shoppingGirl.png";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: shoppingGirl,
+    title: "SUMMER SALE",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT OFF FOR NEW ARRIVALS.",
+  },
+  {
+    id: 2,
+    img: shoppingGirl,
+    title: "AUTUMN COLLECTION",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT OFF FOR NEW ARRIVALS.",
+  },
+  {
+    id: 3,
+    img: shoppingGirl,
+    title: "LOUNGEWEAR LOVE",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT OFF FOR NEW ARRIVALS.",
+  },
+];
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Annoucement from "../components/Annoucement";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Annoucement />
       <Navbar />
+      <Slider />
     </div>
   );
 };
