c27175e574f72a851eaa8c225962cae97a78be4a
Johnson Bhaskaran <johnsonb@live.in>
2025-09-10 17:01:42 +0530

    NewComp: Announcement component created

---
+++ client/src/components/Annoucement.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+`;
+
+const Annoucement = () => {
+  return <Container>Annoucement</Container>;
+};
+export default Annoucement;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Annoucement from "../components/Annoucement";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Annoucement />
     </div>
   );
 };
