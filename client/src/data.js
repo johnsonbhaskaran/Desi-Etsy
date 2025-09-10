5bfd002de72627ed5f47abffc73de258dcf00565
Johnson Bhaskaran <johnsonb@live.in>
2025-09-10 19:47:48 +0530

    Update: categories data hardcoded in data.js

--- client/src/data.js
+++ client/src/data.js
@@ -20,3 +20,21 @@ export const sliderItems = [
     description: "DON'T COMPROMISE ON STYLE! GET FLAT OFF FOR NEW ARRIVALS.",
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexel-photo-5886041.jpg",
+    title: "SHIRT STYLE",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexel-photo-2983464.jpg",
+    title: "LOUNGEWEAR LOVE!",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexel-photo-5480696.jpg",
+    title: "LIGHT JACKETS",
+  },
+];
