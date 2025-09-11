133d70a843a97308a04e12c001c510d256316b7c
Johnson Bhaskaran <johnsonb@live.in>
2025-09-11 13:29:35 +0530

    Done: Footer component completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,137 @@
 import styled from "styled-components";
+import Facebook from "@mui/icons-material/Facebook";
+import Instagram from "@mui/icons-material/Instagram";
+import X from "@mui/icons-material/X";
+import Pinterest from "@mui/icons-material/Pinterest";
+import RoomIcon from "@mui/icons-material/Room";
+import PhoneIcon from "@mui/icons-material/Phone";
+import MailOutlineOutlinedIcon from "@mui/icons-material/MailOutlineOutlined";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Descp = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  color: wheat;
+  background-color: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
 
 const Footer = () => {
-  return <Container>Footer</Container>;
+  return (
+    <Container>
+      <Left>
+        <Logo>Desi Etsy.</Logo>
+        <Descp>
+          There are many variations of passages of Lorem Ipsum available, but the majority have
+          suffered alteration in some form, by injected humour, or randomised words which don't look
+          even slightly believable.
+        </Descp>
+        <SocialContainer>
+          <SocialIcon>
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon>
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon>
+            <X />
+          </SocialIcon>
+          <SocialIcon>
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Men Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <RoomIcon style={{ marginRight: 10 }} /> 622 Dixie Path, South Tobinchester 98336
+        </ContactItem>
+        <ContactItem>
+          <PhoneIcon style={{ marginRight: 10 }} />
+          +1 234 56 78
+        </ContactItem>
+        <ContactItem>
+          <MailOutlineOutlinedIcon style={{ marginRight: 10 }} />
+          helpdesk@desi-etsy.com
+        </ContactItem>
+        <Payment src='https://i.ibb.co/Qfvn4z6/payment.png' />
+      </Right>
+    </Container>
+  );
 };
 export default Footer;
