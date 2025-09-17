432ab9a4b6e78aec3f403fef9ba83892986d5771
Johnson Bhaskaran <johnsonb@live.in>
2025-09-17 12:31:11 +0530

    Done: User schema created

---
+++ ecommerceapi/models/User.js
@@ -0,0 +1,13 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default User = model("User", userSchema);
