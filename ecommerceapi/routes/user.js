29f5fd683d25975856a981471a39682cfcaf522c
Johnson Bhaskaran <johnsonb@live.in>
2025-09-17 12:49:19 +0530

    Remove: unwanted experiments code

--- ecommerceapi/routes/user.js
+++ ecommerceapi/routes/user.js
@@ -1,13 +1,3 @@
-import express, { json } from "express";
+import express from "express";
 
 export const router = express.Router();
-
-router.get("/usertest", (req, res) => {
-  res.json({ message: "User test successfully" });
-});
-
-router.post("/userposttest", (req, res) => {
-  const username = req.body.username;
-
-  res.json({ message: `User ${username} created successfully` });
-});
