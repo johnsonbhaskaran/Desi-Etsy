4ef90cbda9f0a499d34ac26ddb37a83814dbc700
Johnson Bhaskaran <johnsonb@live.in>
2025-09-17 12:34:39 +0530

    MInor: Just added timestamps for creation time tracking

--- ecommerceapi/models/User.js
+++ ecommerceapi/models/User.js
@@ -1,13 +1,16 @@
 import { model, Schema } from "mongoose";
 
-const userSchema = Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  isAdmin: {
-    type: Boolean,
-    default: false,
+const userSchema = Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  { timestamps: true }
+);
 
 export default User = model("User", userSchema);
