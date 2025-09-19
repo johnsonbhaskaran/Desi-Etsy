20bba7dccafeb9b8f3f5ecc3271034c82f65d6c7
Johnson Bhaskaran <johnsonb@live.in>
2025-09-19 07:29:10 +0530

    Update: logic added to extract token from req header

--- ecommerceapi/routes/verifyToken.js
+++ ecommerceapi/routes/verifyToken.js
@@ -4,7 +4,9 @@ const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
 
   if (authHeader) {
-    jwt.verify(authHeader, process.env.TOKEN_SECRET_KEY, (err, user) => {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.TOKEN_SECRET_KEY, (err, user) => {
       if (err) res.status(403).json("Token is not valid");
       req.user = user;
       next();
@@ -14,4 +16,24 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-export default verifyToken;
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
+
+export { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin };
