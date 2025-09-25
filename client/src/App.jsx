ef9d49ee2797816d0490c9a78bdd4f5ed0cc4c63
Johnson Bhaskaran <johnsonb@live.in>
2025-09-25 07:32:57 +0530

    Modify: now user data is fetched from redux store using useSelector hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,30 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
-  return <Home />;
+  const user = useSelector((state) => state.user.currentUser);
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      {user ? (
+        <Route path='/register' element={<Home />} />
+      ) : (
+        <Route path='/register' element={<Register />} />
+      )}
+      //? While there is a user logged in - will be redirected to Home
+      {user ? (
+        <Route path='/login' element={<Home />} />
+      ) : (
+        <Route path='/login' element={<Login />} />
+      )}
+      <Route path='/products/:category' element={<ProductsPage />} />
+      <Route path='/product/:id' element={<ProductDetail />} />
+      <Route path='/cart' element={<Cart />} />
+    </Routes>
+  );
 }
 
 export default App;
