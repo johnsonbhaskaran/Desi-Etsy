75e2afd44156f77ee519c31436f7bfed5d59da2f
Johnson Bhaskaran <johnsonb@live.in>
2025-09-25 07:35:52 +0530

    Add: App component passed redux provider, store and Persist, Persist used for retaining state even after reload

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/lib/integration/react.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
